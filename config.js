9c21e81547acb85066ef5915c16a63de44e8033a
HASHAN-MD-TEST <Hashanmd2025@gmail.com>
2025-07-14 20:37:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({
-    path: './config.env'
-});
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
-
 module.exports = {
-    SESSION_ID: false
+SESSION_ID: process.env.SESSION_ID || "Q4AjUbLQ#18HDoz61VAXpNqAq7vsrZM8gYLAXyK2aYxA6TuBrICg",
+ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
 };
